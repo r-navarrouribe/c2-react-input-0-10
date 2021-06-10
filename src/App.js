0ce554f868b6f234c8054e72ca029c0b9c6056ea
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-10 20:50:58 +0200

    ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+
 function App() {
+  const [numero, setNumero] = useState(0);
+  const [mensaje, setMensaje] = useState("");
+  const sumarNumero = () => {
+    if (numero === 10) {
+      const nuevoMensaje = "La avaricia rompe el saco";
+      setMensaje(nuevoMensaje);
+      return;
+    }
+    const nuevoMensaje = "";
+    setMensaje(nuevoMensaje);
+    setNumero(numero + 1);
+  };
+  const restarNumero = () => {
+    if (numero === 0) {
+      const nuevoMensaje = "Será mejor espavilar";
+      setMensaje(nuevoMensaje);
+      return;
+    }
+    const nuevoMensaje = "";
+    setMensaje(nuevoMensaje);
+    setNumero(numero - 1);
+  };
   return (
-    <div className="App">
-      <p>hola</p>
-    </div>
+    <>
+      <div className="contenedor">
+        <button className="boton" onClick={restarNumero}>
+          -
+        </button>
+        <input type="number" className="input-numero" disabled value={numero} />
+        <button className="boton" onClick={sumarNumero}>
+          +
+        </button>
+        <div>
+          <p className="mensaje">{mensaje}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
